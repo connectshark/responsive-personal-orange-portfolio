92f0e31416280ce8952b4cef8551057cb5e32fbf
Chambers <bobhus394@gmail.com>
2022-08-09 14:24:26 +0800

    微調基本配置

--- assets/js/menuHandler.js
+++ assets/js/menuHandler.js
@@ -1,27 +1,16 @@
 const menu = document.querySelector('.menu')
 const closeBtn = document.querySelector('.close')
 const aside = document.querySelector('.aside')
-
+const navItem = document.querySelectorAll('.nav-item')
 
 const toggleMenu = () => {
   aside.classList.toggle('active')
 }
 
+const closeMenu = () => {
+  aside.classList.remove('active')
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+navItem.forEach(item => item.addEventListener('click', closeMenu))
 menu.addEventListener('click', toggleMenu)
 closeBtn.addEventListener('click', toggleMenu)
-
-
